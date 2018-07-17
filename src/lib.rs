59297867163b6b04ca41b32788a7205e1e8b8ceb
Clay Molocznik <clay@pointc.io>
2018-07-17 20:47:36 +1000

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![feature(test)]
+
 ///  Representation of a radix tree as implemented in this file, that contains
 ///  the strings "foo", "foobar" and "footer" after the insertion of each
 ///  word. When the node represents a key inside the radix tree, we write it
@@ -57,8 +60,6 @@
 ///  is created (the chain must also not include nodes that represent keys),
 ///  it must be compressed back into a single node.
 
-#![allow(dead_code)]
-#![feature(test)]
 
 extern crate libc;
 extern crate nix;
